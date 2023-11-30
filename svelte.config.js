b6fb09c4aa9cfdbdb0ad899670eb1833e6fe46d6
Jose Celano <josecelano@gmail.com>
2023-11-30 13:37:39 +0000

    feat: add workflow to deploy to gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,46 @@
-import adapter from '@sveltejs/adapter-auto'
-import { vitePreprocess } from '@sveltejs/kit/vite'
-import { mdsvex} from 'mdsvex'
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
-/** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-	extensions: ['.md'],
-}
+const extensions = ['.svelte', '.md'];
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions:['.svelte', '.md'],
-	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
-
 	kit: {
-		adapter: adapter()
-	}
-}
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: 'warn'
+		}
+	},
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: extensions,
+			rehypePlugins: [
+				rehypeExternalLinks, // Adds 'target' and 'rel' to external links
+				rehypeSlug, // Adds 'id' attributes to Headings (h1,h2,etc)
+				[
+					rehypeAutolinkHeadings,
+					{
+						// Adds hyperlinks to the headings, requires rehypeSlug
+						behavior: 'prepend',
+						properties: { className: ['heading-link'], title: 'Permalink', ariaHidden: 'true' },
+						content: {
+							type: 'element',
+							tagName: 'span',
+							properties: {},
+							children: [{ type: 'text', value: '#' }]
+						}
+					}
+				]
+			]
+		})
+	],
+	extensions: extensions
+};
 
-export default config
+export default config;
